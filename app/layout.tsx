df5d34b71deace766c0f6fb2136df4f57ed1dab9
MathPrm <primotmathilde@gmail.com>
2026-09-07 14:20:16 +0200

    fixed layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font, @next/next/google-font-display */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+        />
+      </head>
       <body className={`${raleway.variable} ${lato.variable} ${poppins.variable} font-poppins antialiased bg-page text-text-primary`}>
         <Header />
 
